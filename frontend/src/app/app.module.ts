d20819817cdd2d22666d3318c01f40976780cdf4
conradtrost <ct.conradtrost@gmail.com>
2021-10-25 00:30:55 -0500

    Frontend added for posting and viewing posts

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { PostsComponent } from './posts/posts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend/src/app/posts/posts.component.ts
@@ -0,0 +1,44 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+
+@Component({
+    selector: 'app-posts',
+    templateUrl: './posts.component.html',
+    styleUrls: ['./posts.component.css'],
+    host: {'class': 'chat-area'}
+})
+export class PostsComponent {
+  constructor(private http: HttpClient, private formBuilder: FormBuilder) {
+    this.loadPosts();
+  }
+
+  inputForm = this.formBuilder.group({
+    body: ''
+  })
+
+  posts: any[] = [];
+
+  loadPosts(): void {
+    this.http.get('http://127.0.0.1:3000/posts')
+    .subscribe((posts: any) => {
+      this.posts = posts;
+      setTimeout(() => {
+        const chatClient = document.getElementById('chat-responses');
+        if (chatClient) {
+          chatClient.scrollTop = chatClient.scrollHeight;
+        }
+      }, 50)
+
+    });
+  }
+
+  createPost(): void {
+    this.http.post('http://127.0.0.1:3000/posts', {
+      body: this.inputForm.value.body
+    }).subscribe((res: any) => {
+      this.inputForm.reset();
+      this.loadPosts();
+    }, (error)=>alert(JSON.stringify(error)));
+  }
+}
